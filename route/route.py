988c390445aa86705cad6daae9f19f4f320f39fa
ycl <931415151@qq.com>
2023-11-24 11:51:44 +0000

    pait all xp

--- route/route.py
+++ route/route.py
@@ -72,64 +72,69 @@ def get_xp():
                 result = match.groups()
                 port = int(result[0])
                 dir = result[1]
+                
+                if dir == "east":
+                    ws.cell(row=row_num, column=col_num + 1).alignment = alignment
+                    ws.cell(row=row_num, column=col_num + 1).border = border
+                    ws.cell(row=row_num, column=col_num + 2).value = "→"
+                    ws.cell(row=row_num, column=col_num + 2).alignment = alignment
+                    ws.cell(row=row_num, column=col_num + 2).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
+                    ws.cell(row=row_num + 1, column=col_num + 1).alignment = alignment
+                    ws.cell(row=row_num + 1, column=col_num + 1).border = border
+                
+                if dir == "west":
+                    ws.cell(row=row_num + 1, column=col_num - 2).alignment = alignment
+                    ws.cell(row=row_num + 1, column=col_num - 2).border = border
+                    ws.cell(row=row_num + 1, column=col_num - 3).value = "←"
+                    ws.cell(row=row_num + 1, column=col_num - 3).alignment = alignment
+                    ws.cell(row=row_num + 1, column=col_num - 3).fill = PatternFill(fill_type='solid', start_color='87CEEB')
+                    ws.cell(row=row_num, column=col_num - 2).alignment = alignment
+                    ws.cell(row=row_num, column=col_num - 2).border = border
+                
+                if dir == "south":
+                    ws.cell(row=row_num + 2, column=col_num).alignment = alignment
+                    ws.cell(row=row_num + 2, column=col_num).border = border
+                    ws.cell(row=row_num + 3, column=col_num).value = "↓"
+                    ws.cell(row=row_num + 3, column=col_num).alignment = alignment
+                    ws.cell(row=row_num + 3, column=col_num).fill = PatternFill(fill_type='solid', start_color='87CEEB')
+                    ws.cell(row=row_num + 2, column=col_num  - 1).alignment = alignment
+                    ws.cell(row=row_num + 2, column=col_num  - 1).border = border
+                
+                if dir == "north":
+                    ws.cell(row=row_num - 1, column=col_num - 1).alignment = alignment
+                    ws.cell(row=row_num - 1, column=col_num - 1).border = border
+                    ws.cell(row=row_num - 2, column=col_num - 1).value = "↑"
+                    ws.cell(row=row_num - 2, column=col_num - 1).alignment = alignment
+                    ws.cell(row=row_num - 2, column=col_num - 1).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
+                    ws.cell(row=row_num - 1, column=col_num).alignment = alignment
+                    ws.cell(row=row_num - 1, column=col_num).border = border
 
                 for p in port_coordinates:
                     if p[1] == port:
-                        print(p[0], p[1], dir, p[2])
                         if dir == "east" and p[0] == "tx":
                             ws.cell(row=row_num, column=col_num + 1).value = p[2]
-                            ws.cell(row=row_num, column=col_num + 1).alignment = alignment
-                            ws.cell(row=row_num, column=col_num + 1).border = border
-                            ws.cell(row=row_num, column=col_num + 2).value = "→"
-                            ws.cell(row=row_num, column=col_num + 2).alignment = alignment
-                            ws.cell(row=row_num, column=col_num + 2).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
                         if dir == "east" and p[0] == "rx":
                             ws.cell(row=row_num + 1, column=col_num + 1).value = p[2]
-                            ws.cell(row=row_num + 1, column=col_num + 1).alignment = alignment
-                            ws.cell(row=row_num + 1, column=col_num + 1).border = border
-                        
                         if dir == "west" and p[0] == "tx":
                             ws.cell(row=row_num + 1, column=col_num - 2).value = p[2]
-                            ws.cell(row=row_num + 1, column=col_num - 2).alignment = alignment
-                            ws.cell(row=row_num + 1, column=col_num - 2).border = border
-                            ws.cell(row=row_num + 1, column=col_num - 3).value = "←"
-                            ws.cell(row=row_num + 1, column=col_num - 3).alignment = alignment
-                            ws.cell(row=row_num + 1, column=col_num - 3).fill = PatternFill(fill_type='solid', start_color='87CEEB')
                         if dir == "west" and p[0] == "rx":
                             ws.cell(row=row_num, column=col_num - 2).value = p[2]
-                            ws.cell(row=row_num, column=col_num - 2).alignment = alignment
-                            ws.cell(row=row_num, column=col_num - 2).border = border
-                        
                         if dir == "south" and p[0] == "tx":
                             ws.cell(row=row_num + 2, column=col_num).value = p[2]
-                            ws.cell(row=row_num + 2, column=col_num).alignment = alignment
-                            ws.cell(row=row_num + 2, column=col_num).border = border
-                            ws.cell(row=row_num + 3, column=col_num).value = "↓"
-                            ws.cell(row=row_num + 3, column=col_num).alignment = alignment
-                            ws.cell(row=row_num + 3, column=col_num).fill = PatternFill(fill_type='solid', start_color='87CEEB')
                         if dir == "south" and p[0] == "rx":
-                            ws.cell(row=row_num - 1, column=col_num  + 2).value = p[2]
-                            ws.cell(row=row_num - 1, column=col_num  + 2).alignment = alignment
-                            ws.cell(row=row_num - 1, column=col_num  + 2).border = border
-                        
+                            ws.cell(row=row_num + 2, column=col_num  - 1).value = p[2]
                         if dir == "north" and p[0] == "tx":
                             ws.cell(row=row_num - 1, column=col_num - 1).value = p[2]
-                            ws.cell(row=row_num - 1, column=col_num - 1).alignment = alignment
-                            ws.cell(row=row_num - 1, column=col_num - 1).border = border
-                            ws.cell(row=row_num - 1, column=col_num - 2).value = "↑"
-                            ws.cell(row=row_num - 1, column=col_num - 2).alignment = alignment
-                            ws.cell(row=row_num - 1, column=col_num - 2).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
                         if dir == "north" and p[0] == "rx":
-                            ws.cell(row=row_num, column=col_num  - 1).value = p[2]
-                            ws.cell(row=row_num, column=col_num  - 1).alignment = alignment
-                            ws.cell(row=row_num, column=col_num  - 1).border = border
+                            ws.cell(row=row_num - 1, column=col_num).value = p[2]
+                        
+                        print(p[0], p[1], dir, p[2])
+                    
 
 def pait_xp():
     for row in xy_coordinates:
         x = row[0]
         y = row[1]
-
-        print(x, y)
 
         cell1 = ws.cell(row=x, column=y)
         cell2 = ws.cell(row=x+1, column=y)
@@ -141,6 +146,8 @@ def pait_xp():
         ws.cell(row=x, column=y).font      = font
 
         #ws.cell(row=x, column=y).value = "↑"
+        #ws.row_dimensions[row_num + 3].height=100
+        #ws.column_dimensions[col_num - 3].width=50
 
 
 def main():
