281b423e58a9b0e58e625112002cabbcdabf76fb
ycl <931415151@qq.com>
2023-11-27 02:18:35 +0000

    we can show it

--- route/route.py
+++ route/route.py
@@ -80,7 +80,7 @@ def get_xp():
                     ws.cell(row=row_num, column=col_num + 1).border = border
                     ws.cell(row=row_num, column=col_num + 2).value = "→"
                     ws.cell(row=row_num, column=col_num + 2).alignment = alignment
-                    ws.cell(row=row_num, column=col_num + 2).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
+                    ws.cell(row=row_num, column=col_num + 2).fill = PatternFill(fill_type='solid', start_color='FCE4D6')
                     ws.cell(row=row_num + 1, column=col_num + 1).alignment = alignment
                     ws.cell(row=row_num + 1, column=col_num + 1).border = border
                 
@@ -91,7 +91,7 @@ def get_xp():
                     ws.cell(row=row_num + 1, column=col_num - 2).border = border
                     ws.cell(row=row_num + 1, column=col_num - 3).value = "←"
                     ws.cell(row=row_num + 1, column=col_num - 3).alignment = alignment
-                    ws.cell(row=row_num + 1, column=col_num - 3).fill = PatternFill(fill_type='solid', start_color='87CEEB')
+                    ws.cell(row=row_num + 1, column=col_num - 3).fill = PatternFill(fill_type='solid', start_color='E2EFDA')
                     ws.cell(row=row_num, column=col_num - 2).alignment = alignment
                     ws.cell(row=row_num, column=col_num - 2).border = border
                 
@@ -102,7 +102,7 @@ def get_xp():
                     ws.cell(row=row_num + 2, column=col_num).border = border
                     ws.cell(row=row_num + 3, column=col_num).value = "↓"
                     ws.cell(row=row_num + 3, column=col_num).alignment = alignment
-                    ws.cell(row=row_num + 3, column=col_num).fill = PatternFill(fill_type='solid', start_color='87CEEB')
+                    ws.cell(row=row_num + 3, column=col_num).fill = PatternFill(fill_type='solid', start_color='E2EFDA')
                     ws.cell(row=row_num + 2, column=col_num  - 1).alignment = alignment
                     ws.cell(row=row_num + 2, column=col_num  - 1).border = border
                 
@@ -113,9 +113,25 @@ def get_xp():
                     ws.cell(row=row_num - 1, column=col_num - 1).border = border
                     ws.cell(row=row_num - 2, column=col_num - 1).value = "↑"
                     ws.cell(row=row_num - 2, column=col_num - 1).alignment = alignment
-                    ws.cell(row=row_num - 2, column=col_num - 1).fill = PatternFill(fill_type='solid', start_color='FFC0CB')
+                    ws.cell(row=row_num - 2, column=col_num - 1).fill = PatternFill(fill_type='solid', start_color='FCE4D6')
                     ws.cell(row=row_num - 1, column=col_num).alignment = alignment
                     ws.cell(row=row_num - 1, column=col_num).border = border
+                
+                if dir == "inter":
+                    ws.cell(row=row_num - 1, column=col_num - 1).value = 0
+                    ws.cell(row=row_num - 1, column=col_num).value = 0
+                    ws.cell(row=row_num - 1, column=col_num - 1).alignment = alignment
+                    ws.cell(row=row_num - 1, column=col_num - 1).border = border
+                    ws.cell(row=row_num - 2, column=col_num - 1).value = "↑"
+                    ws.cell(row=row_num - 2, column=col_num - 1).alignment = alignment
+                    ws.cell(row=row_num - 2, column=col_num - 1).fill = PatternFill(fill_type='solid', start_color='FCE4D6')
+                    ws.cell(row=row_num - 1, column=col_num).alignment = alignment
+                    ws.cell(row=row_num - 1, column=col_num).border = border
+                    ws.cell(row=row_num - 2, column=col_num).value = "↓"
+                    ws.cell(row=row_num - 2, column=col_num).alignment = alignment
+                    ws.cell(row=row_num - 2, column=col_num).fill = PatternFill(fill_type='solid', start_color='E2EFDA')
+                    
+
 
                 for p in port_coordinates:
                     if p[1] == port:
@@ -135,11 +151,16 @@ def get_xp():
                             ws.cell(row=row_num - 1, column=col_num - 1).value = p[2]
                         if dir == "north" and p[0] == "rx":
                             ws.cell(row=row_num - 1, column=col_num).value = p[2]
+                        if dir == "inter" and p[0] == "tx":
+                            ws.cell(row=row_num - 1, column=col_num - 1).value = p[2]
+                        if dir == "inter" and p[0] == "rx":
+                            ws.cell(row=row_num - 1, column=col_num).value = p[2]
                         
                         print(p[0], p[1], dir, p[2])
                     
 
 def pait_xp():
+    ws.row_dimensions[2].height=70.5
     for row in xy_coordinates:
         x = row[0]
         y = row[1]
@@ -153,10 +174,8 @@ def pait_xp():
         ws.cell(row=x, column=y).border    = border
         ws.cell(row=x, column=y).font      = font
 
-        #ws.cell(row=x, column=y).value = "↑"
-        #ws.row_dimensions[row_num + 3].height=100
-        #ws.column_dimensions[col_num - 3].width=50
-
+        ws.row_dimensions[x + 3].height=70.5
+        ws.column_dimensions[(chr(y + 2 + 64))].width=14.5
 
 def main():
     get_xp()
